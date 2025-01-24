8eb139a25ebc793c0a6b4a7b3294383fee54b99a
LenonAnthony <lenonanthony77@icloud.com>
2025-01-24 15:56:39 -0300

    remove js

--- image_extractor_js/__tests__/config.test.js
+++
@@ -1,8 +0,0 @@
-const { cfg } = require("../image-extractor/config");
-
-test("configuration test", () => {
-  expect(cfg.chatOpenAI).toBeTruthy();
-  expect(cfg.geminiAPIKey).toBeTruthy();
-  expect(cfg.googleAI).toBeTruthy();
-  expect(cfg.googleModel).toBeTruthy();
-});
--- image_extractor_js/test.ps1
+++
@@ -1,8 +0,0 @@
-echo "node .\image-extractor\main.js -f .\images\ -m google -e jfif"
-node .\image-extractor\main.js -f .\images\ -m google -e jfif
-echo "node .\image-extractor\main.js --folder .\images\ --model google --extension jfif --batch_size 2"
-node .\image-extractor\main.js --folder .\images\ --model google --extension jfif --batch_size 2
-echo "node .\image-extractor\main.js --folder .\images\ --model openai --extension jfif"
-node .\image-extractor\main.js --folder .\images\ --model openai --extension jfif
-echo "node .\image-extractor\main.js --folder .\images\ --model openai --extension jfif --batch_size 2"
-node .\image-extractor\main.js --folder .\images\ --model openai --extension jfif --batch_size 2
